94b95c03aa9b3be66515ef69bd2374123e52cbb0
aescolastico <aesubs@icloud.com>
2020-06-11 18:34:28 -0400

    Update win10-iso-functions.ps1

--- win10-iso-functions.ps1
+++ win10-iso-functions.ps1
@@ -182,3 +182,29 @@ function Start-Win10Upgrade {
     Write-Host "The Upgrade will commence shortly. Your PC will be rebooted. Please save any work you do not want to lose."
     Invoke-Expression "$DLPath /copylogs $LogPath /auto upgrade /dynamicupdate /compat ignorewarning enable /skipeula /quietinstall"
 }
+function Install-WinCAB{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [String] $Link,
+        [Parameter(Mandatory=$false)]
+        [String] $Path = (Get-Location).Path
+    )
+    $KB = ($Link | Select-String -Pattern 'kb(\d+)').Matches.Value
+    $FileName = "$KB-patch.cab"
+    $CabFilePath = "$Path\$FileName"
+    (New-Object System.Net.WebClient).DownloadFile($Link, $CabFilePath)
+    Invoke-Expression "DISM.exe /Online /Add-Package /Silent /PackagePath:$CabFilePath"
+}
+function Start-Win10UpgradeCAB{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false)] 
+        [ValidateSet("1909")]
+        [String] $Version = "1909"
+    )
+    if($Version -eq "1909"){
+        $Link = 'http://b1.download.windowsupdate.com/d/upgr/2019/11/windows10.0-kb4517245-x64_4250e1db7bc9468236c967c2c15f04b755b3d3a9.cab'
+    }
+    Install-WinCAB -Link $Link
+}
